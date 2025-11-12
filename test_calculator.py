ff070a935e32aa6bb5ae91f6b96fc049b42d0695
gluGPU <william.hudspeth@icloud.com>
2025-11-12 15:48:23 -0500

    Final Changes.

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(10, mul(2, 5))
+        self.assertAlmostEqual(0.333, mul(1, 0.333))
+        self.assertEqual(1000, mul(100, 10))
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(div(2, 10), 5)
+        self.assertAlmostEqual(div(10, 5), 0.5)
+        self.assertEqual(div(2, 8), 4)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,23 +37,23 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            log(0, 5)
+    
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(10.44031, hypotenuse(3, 10), 4)
+        self.assertEqual(5, hypotenuse(4, 3))
+        self.assertAlmostEqual(22.36068, hypotenuse(20, 10), 4)
+    
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        
+        self.assertEqual(0, square_root(0))
+        self.assertEqual(10, square_root(100))
 
 # Do not touch this
+# im gonna touch it
 if __name__ == "__main__":
     unittest.main()
